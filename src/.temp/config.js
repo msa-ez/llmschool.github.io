9dc6201378cf42336929e238c7ed07b6f1dac75d
kibumpark <top5w5@naver.com>
2024-12-03 16:07:45 +0900

    chore: update .gitignore

--- src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - llmschool",
-  "siteUrl": "https://llmschool.github.io",
-  "version": "0.7.23",
-  "catchLinks": true
-}
